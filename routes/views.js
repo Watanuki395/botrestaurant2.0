d9d7c854505b98cff02e3d8f6caf57989eff8d4e
Gerardo Salas Montoya <gesalas@kyndryl.com>
2021-12-04 13:39:40 -0600

    adding new view

--- routes/views.js
+++ routes/views.js
@@ -3,39 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/menu", async (req, res) => {
-  return res.render('menu');
+    return res.render('menu');
 });
 
-router.get("/inventarios", async (req, res) => {
-  return res.render('sys_invent');
+router.get("/breakfast", async (req, res) => {
+    return res.render('breakfast');
 });
 
-router.get("/rest", async (req, res) => {
-  return res.render('sys_rest');
-});
-
-router.get("/factu", async (req, res) => {
-  return res.render('sys_factu');
-});
-
-router.get("/educ", async (req, res) => {
-  return res.render('sys_educ');
-});
-
-router.get("/sysemails", async (req, res) => {
-  return res.render('sys_email');
-});
-
-router.get("/custm", async (req, res) => {
-  return res.render('sys_custm');
-});
-
-router.get("/info", async (req, res) => {
-  return res.render('req_info');
-});
-router.get("/cmpltd", async (req, res) => {
-  return res.render('req_cmpltd');
-});
 
 router.post("/set-info", async (req, res) => {
 
